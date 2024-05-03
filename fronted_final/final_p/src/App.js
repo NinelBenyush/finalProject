b1cd8829aee2db96ad1b5ac2832795f7c42278b7
NinelBenyush <ninelbenush@gmail.com>
2024-05-03 15:27:19 +0300

    /

---
+++ fronted_final/final_p/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ResponsiveAppBar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar/>
+    </div>
+  );
+}
+
+export default App;
