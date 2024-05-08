88052a1fe9ef37f7986f379e47e16acd06876521
NinelBenyush <ninelbenush@gmail.com>
2024-05-08 09:12:21 +0300

    /

--- backend.py
+++ backend.py
@@ -1,15 +1,27 @@
 import pandas as pd
+import numpy as np
 
 
 class prepare_LSTM():
     def __init__(self, data):
         self.data = data
+        self.seq = 3
 
-    def split_data(self):
+    def split_data_and_seq(self):
+        new_data = np.array(self.data)
+        x_data, y_data = [], []
+
+        for i in range(len(new_data)- self.seq+1):
+            x_data.append(new_data[i:i + self.seq])
+            y_data.append(new_data[i + self.seq-1])
+
+        x_data, y_data = np.array(x_data), np.array(y_data)
+
         train_size = int(len(self.data) * 0.8)
-        train_data = self.data[:train_size]
-        test_data = self.data[train_size:]
-        return train_data, test_data
+        x_train, y_train = x_data[:train_size], y_data[:train_size]
+        x_test, y_test = x_data[train_size:], y_data[train_size:]
+
+        return x_train, y_train, x_test, y_test
 
 
 class main:
@@ -22,7 +34,12 @@ class main:
     final_data = data.set_index("Month")
 
     prepare_to_model = prepare_LSTM(final_data)
-    x_train, x_test = prepare_to_model.split_data()
+    x_train, y_train, x_test, y_test = prepare_to_model.split_data_and_seq()
+    print(x_train.shape)
+    print(y_train.shape)
+    print(x_test.shape)
+    print(y_test.shape)
+
 
 
 if __name__ == "__main__":
