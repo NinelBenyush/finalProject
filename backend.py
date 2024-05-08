ce1b7765a7d89b250fb0b5719ccaf4cbd24ab35a
NinelBenyush <ninelbenush@gmail.com>
2024-05-08 12:01:32 +0300

    .

--- backend.py
+++ backend.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+import tensorflow as tf
 
 
-class prepare_LSTM():
+class prepare_LSTM:
     def __init__(self, data):
         self.data = data
         self.seq = 3
+        self.num_features = 1
 
     def split_data_and_seq(self):
-        new_data = np.array(self.data)
+        new_data = np.array(self.data['Value'])
         x_data, y_data = [], []
 
-        for i in range(len(new_data)- self.seq+1):
+        for i in range(len(new_data) - self.seq+1):
             x_data.append(new_data[i:i + self.seq])
             y_data.append(new_data[i + self.seq-1])
 
@@ -23,9 +27,24 @@ class prepare_LSTM():
 
         return x_train, y_train, x_test, y_test
 
+class LSTM:
+    def __init__(self):
+        self.num_features = 1
+        self.seq = 3
+        self.model = self.create_lstm()
+
+    def create_lstm(self):
+        model = Sequential([
+            LSTM(1, activation='relu', input_shape=(self.seq_length, self.num_features)),
+            Dense(1)
+        ])
+        return model
+
+
 
 class main:
     data = pd.read_csv("prediction_data.csv")
+    print(tf.__version__)
 
     month_map = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
                  7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
@@ -35,11 +54,7 @@ class main:
 
     prepare_to_model = prepare_LSTM(final_data)
     x_train, y_train, x_test, y_test = prepare_to_model.split_data_and_seq()
-    print(x_train.shape)
-    print(y_train.shape)
-    print(x_test.shape)
-    print(y_test.shape)
-
+    lstm_model = LSTM()
 
 
 if __name__ == "__main__":
