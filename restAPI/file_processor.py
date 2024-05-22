55bbedf1011d084d5fbd44ff14cd802077e9f75e
NinelBenyush <ninelbenush@gmail.com>
2024-05-22 10:15:34 +0300

    .

--- restAPI/file_processor.py
+++ restAPI/file_processor.py
@@ -1,8 +1,26 @@
 import os
 import pandas as pd
 
+counter_file = "count_for_data.txt"
+
+def get_num():
+    with open(counter_file, 'r') as f:
+        count = int(f.read().strip())
+    count += 1
+
+    with open(counter_file, 'w') as f:
+        f.write(str(count))
+
+    return count
+
 def work_on_file(filePath):
     print(f"name of the file: {filePath}")
 
+    file_num = get_num()
+
+    file_name = f"data{file_num}.csv"
+
+
     df = pd.read_excel(filePath)
-    df.to_csv("data.csv")
+    location = os.path.join("./DataForPrediction", file_name)
+    df.to_csv(location)
