40bf7092984ca63a4fbfd92e9514535c740b060c
NinelBenyush <ninelbenush@gmail.com>
2024-05-28 16:13:42 +0300

    /

--- try.py
+++ try.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import torch
 import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.utils.data import DataLoader
 
 # Read data
 data = pd.read_csv("final_data_prediction.csv")
@@ -71,6 +73,7 @@ y_train = torch.FloatTensor(y_train)
 X_test = torch.FloatTensor(X_test).unsqueeze(2)  # Add a dimension for features
 y_test = torch.FloatTensor(y_test)
 
+"""
 # Print shapes
 print("Train shape:", X_train.shape, y_train.shape)
 print("Test shape:", X_test.shape, y_test.shape)
@@ -120,3 +123,85 @@ plot_sequences(X_train, y_train)
 
 print("Visualizing Testing Data:")
 plot_sequences(X_test, y_test)
+"""
+def create_lstm_model(input_size, hidden_size, num_layers):
+    class LSTMModel(nn.Module):
+        def __init__(self, input_size, hidden_size, num_layers):
+            super(LSTMModel, self).__init__()
+            self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+            self.linear = nn.Linear(hidden_size, 1)
+
+        def forward(self, x):
+            out, _ = self.lstm(x)
+            out = self.linear(out[:, -1, :])
+            return out
+
+    return LSTMModel(input_size, hidden_size, num_layers)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+input_size = 1
+num_layers = 2
+hidden_size = 64
+output_size = 1
+model = create_lstm_model(input_size, hidden_size, num_layers).to(device)
+loss_fn = torch.nn.MSELoss(reduction='mean')
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+batch_size = 12
+
+train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+def train_lstm_model(model, train_loader, test_loader, num_epochs, optimizer, loss_fn, device):
+    train_hist = []
+    test_hist = []
+    
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        
+        # Training
+        model.train()
+        for batch_X, batch_y in train_loader:
+            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+            predictions = model(batch_X)
+            loss = loss_fn(predictions, batch_y)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            total_loss += loss.item()
+        
+        # Calculate average training loss
+        average_loss = total_loss / len(train_loader)
+        train_hist.append(average_loss)
+        
+        # Validation on test data
+        model.eval()
+        with torch.no_grad():
+            total_test_loss = 0.0
+            
+            for batch_X_test, batch_y_test in test_loader:
+                batch_X_test, batch_y_test = batch_X_test.to(device), batch_y_test.to(device)
+                predictions_test = model(batch_X_test)
+                test_loss = loss_fn(predictions_test, batch_y_test)
+                
+                total_test_loss += test_loss.item()
+            
+            # Calculate average test loss
+            average_test_loss = total_test_loss / len(test_loader)
+            test_hist.append(average_test_loss)
+        
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}] - Training Loss: {average_loss:.4f}, Test Loss: {average_test_loss:.4f}')
+    
+    return train_hist, test_hist
+
+
+num_epochs = 50
+train_hist, test_hist = train_lstm_model(model, train_loader, test_loader, num_epochs, optimizer, loss_fn, device)
+
